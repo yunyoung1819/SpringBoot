bc224e809f573db509596fda7589528213ec5015
yunyoung1819 <yunyoung1819@hanmail.net>
2018-11-19 17:25:58 +0900

    applied lombok library

--- boot01/src/main/java/org/zerock/controller/SampleController.java
+++ boot01/src/main/java/org/zerock/controller/SampleController.java
@@ -2,14 +2,30 @@ package org.zerock.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.zerock.domain.SampleVO;
 
-@RestController
+@RestController	// REST 방식의 컨트롤러입니다. 
 public class SampleController {
 	
 	@GetMapping("/hello")
 	public String sayHello() {
 		
 		return "Hello World";
+	}
+	
+	// Lombok 적용
+	@GetMapping("/sample")
+	public SampleVO makeSample() {
+		
+		SampleVO vo = new SampleVO();
+		vo.setVal1("Macbook Pro");
+		vo.setVal2("iMac");
+		vo.setVal3("Apple Watch4");
+		
+		System.out.println(vo);
+		
+		return vo;
+		
 	}
 
 }
